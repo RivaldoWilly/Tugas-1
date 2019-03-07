defa96456dcc44ae19097b89d4b2352751dd2877
RivaldoWilly <48308070+RivaldoWilly@users.noreply.github.com>
2019-03-07 13:11:44 +0700

    Update OOP.java

--- OOP.java
+++ OOP.java
@@ -1,12 +1,5 @@
-
-package oop;
-
-
 public class OOP {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
             System.out.println("Nama    : Rivaldo Willy Ramadhanis ");
             System.out.println("Nim     : 17090119");
