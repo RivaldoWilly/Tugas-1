ee1ec76f5dabc5bb1938dd6bda524d2a60ae7354
RivaldoWilly <48308070+RivaldoWilly@users.noreply.github.com>
2019-03-07 13:10:46 +0700

    Add files via upload

---
+++ OOP.java
@@ -0,0 +1,16 @@
+
+package oop;
+
+
+public class OOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Nama    : Rivaldo Willy Ramadhanis ");
+            System.out.println("Nim     : 17090119");
+            System.out.println("Kelas   : 4A");
+    }
+    
+}
